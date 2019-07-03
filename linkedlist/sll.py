0eb50dbd05a61b28011770367d289dfc3e400838
zeus <yash.ahd786@gmail.com>
2019-07-03 14:13:09 +0530

    printlist method for linkedlist

--- linkedlist/sll.py
+++ linkedlist/sll.py
@@ -7,9 +7,16 @@ class Linkedlist:
     def __init__(self):
         self.head = None
 
+    def printlist(self):
+        temp = self.head
+        while temp:
+            print(temp.data)
+            temp = temp.next
+
 if __name__ == "__main__":
     sll = Linkedlist()
     sll.head = Node(2)
     sll.head.next = Node(3)
     sll.head.next.next = Node(4)
+    sll.printlist()
 
